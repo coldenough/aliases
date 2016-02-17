4ebb4ab8edc3153298a8014dd13099ab0751895a
coldenough <vrednopolezno@gmail.com>
2016-02-16 19:11:29 -0800

    Fixed bracess according to best practice

--- Aliases.ps1
+++ Aliases.ps1
@@ -3,13 +3,13 @@
 https://web.archive.org/web/20120213013609/http:
 //huddledmasses.org/powershell-power-user-tips-bash-style-alias-command
 #>
-if ($Host.Version.Major -ge 2) {
+if ( $Host.Version.Major -ge 2 ){
   function script:Resolve-Aliases {
     param ($line)
 
-    [System.Management.Automation.PSParser]::Tokenize($line,[ref]$null) | % {
-      if ($_.Type -eq "Command") {
-        $cmd = @(which $_.Content)[0]
+    [ System.Management.Automation.PSParser ]::Tokenize($line,[ref]$null) | % {
+      if ( $_.Type -eq "Command" ) {
+        $cmd = @( which $_.Content )[0]
         if ($cmd.CommandType -eq "Alias") {
           $line = $line.Remove( $_.StartColumn -1, $_.Length ).
               Insert( $_.StartColumn -1, $cmd.Definition )
